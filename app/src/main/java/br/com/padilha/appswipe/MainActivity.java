f4febbcb13933083484d171c877dfeb162366362
FDAPADILHA <filipe.amaral.padilha@gmail.com>
2021-10-19 02:12:04 -0300

    Adding a functions for métods to swipe

--- app/src/main/java/br/com/padilha/appswipe/MainActivity.java
+++ app/src/main/java/br/com/padilha/appswipe/MainActivity.java
@@ -3,7 +3,9 @@ package br.com.padilha.appswipe;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,5 +20,36 @@ public class MainActivity extends AppCompatActivity {
 
         fundoDaTela = findViewById(R.id.fundoDaTela);
         tvDirecao = findViewById(R.id.tvDirecao);
+
+        fundoDaTela.setOnTouchListener(new OnSwipeTouchListener(this){
+            @Override
+            public void onSwipeBottom() {
+                super.onSwipeBottom();
+                tvDirecao.setText("Baixo");
+                fundoDaTela.setBackgroundColor(Color.GREEN);
+            }
+
+            @Override
+            public void onSwipeTop() {
+                super.onSwipeTop();
+                tvDirecao.setText("Cima");
+                fundoDaTela.setBackgroundColor(Color.BLUE);
+            }
+
+            @Override
+            public void onSwipeLeft() {
+                super.onSwipeLeft();
+                tvDirecao.setText("Esquerda");
+                fundoDaTela.setBackgroundColor(Color.YELLOW);
+            }
+
+            @Override
+            public void onSwipeRight() {
+                super.onSwipeRight();
+                tvDirecao.setText("Direita");
+                fundoDaTela.setBackgroundColor(Color.RED);
+            }
+        });
+
     }
 }
