57c8557bfd9ac1db9ac0b7cc8f562786699d51cb
FDAPADILHA <filipe.amaral.padilha@gmail.com>
2021-10-19 01:47:28 -0300

    Adding Class OnSwipeTouchListener for swipe moves and adding instances of MainActivity objects

--- app/src/main/java/br/com/padilha/appswipe/MainActivity.java
+++ app/src/main/java/br/com/padilha/appswipe/MainActivity.java
@@ -1,14 +1,22 @@
 package br.com.padilha.appswipe;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ConstraintLayout fundoDaTela;
+    private TextView tvDirecao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fundoDaTela = findViewById(R.id.fundoDaTela);
+        tvDirecao = findViewById(R.id.tvDirecao);
     }
 }
